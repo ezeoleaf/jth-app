ee85a3a6390f3d6318e2d0e57d08eb456ce1e2ef
Ezequiel Olea Figueroa <eze.olea.f@gmail.com>
2020-05-16 13:14:44 -0300

    Added register form

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -53,7 +53,7 @@ const useStyles = makeStyles((theme) => ({
 function RegisterForm(props) {
   const classes = useStyles();
   const [state, setState] = useState({
-    email = "",
+    email: "",
     username: "",
     password: "",
     confirmPassword: "",
@@ -62,10 +62,57 @@ function RegisterForm(props) {
 
   const handleChange = (e) => {
     const {id, value} = e.target
-    this.setState(prevState => ({
+    setState(prevState => ({
       ...prevState,
       [id] : value
     }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (state.password === state.confirmPassword) {
+        sendDetailsToServer()
+    } else {
+        alert('Passwords do not match')
+    }
+  }
+
+  const sendDetailsToServer = () => {
+    if (state.email.length && state.username.length && state.password.length) {
+        // props.showError(null)
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var data = new FormData()
+        data.append('email', state.email)
+        data.append('username', state.username)
+        data.append('password', state.password)
+
+        axios.post(API_BASE_URL+'user', data, myHeaders)
+            .then(function (response) {
+                if(response.data.code === 200) {
+                    setState(prevState => ({
+                        ...prevState,
+                        'successMessage': "Registration success"
+                    }))
+                    redirectToLogin()
+                    // props.showError(null)
+                } else {
+                    // props.showError("Some error ocurred")
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+    } else {
+      console.log("aer");
+        // props.showError('Please enter valid username and password')
+    }
+  }
+
+  const redirectToLogin = () => {
+    props.history.push('/login')
   }
 
   return (
@@ -80,30 +127,7 @@ function RegisterForm(props) {
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
+          <Grid item xs={12}>
               <TextField
                 variant="outlined"
                 required
@@ -112,6 +136,21 @@ function RegisterForm(props) {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                value={state.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                value={state.username}
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -124,12 +163,21 @@ function RegisterForm(props) {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={state.password}
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                id="confirmPassword"
+                value={state.confirmPassword}
+                onChange={handleChange}
               />
             </Grid>
           </Grid>
@@ -139,12 +187,14 @@ function RegisterForm(props) {
             variant="contained"
             color="primary"
             className={classes.submit}
+            onSubmit={handleSubmit}
+            onClick={handleSubmit}
           >
             Sign Up
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body2" onClick={redirectToLogin}>
                 Already have an account? Sign in
               </Link>
             </Grid>
@@ -158,149 +208,5 @@ function RegisterForm(props) {
   );
 }
 
-// class RegisterForm extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             email: "",
-//             username: "",
-//             password: "",
-//             confirmPassword: "",
-//             successMessage: null
-//         };
-//     }
-
-//     // handleClick = event => {
-//     // this.setState({anchorEl: event.currentTarget});
-//     // };
-
-//     // handleClose = () => {
-//     // this.setState({anchorEl: null});
-//     // }
-
-//     handleChange = e => {
-//         console.log("CHANGE")
-//         const {id, value} = e.target
-//         this.setState(prevState => ({
-//             ...prevState,
-//             [id] : value
-//         }))
-//     }
-
-//     sendDetailsToServer = () => {
-//         if (this.state.email.length && this.state.username.length && this.state.password.length) {
-//             this.props.showError(null)
-//             var myHeaders = new Headers();
-//             myHeaders.append("Content-Type", "application/json");
-
-//             var data = new FormData()
-//             data.append('email', this.state.email)
-//             data.append('username', this.state.username)
-//             data.append('password', this.state.password)
-
-//             axios.post(API_BASE_URL+'user', data, myHeaders)
-//                 .then(function (response) {
-//                     if(response.data.code === 200) {
-//                         this.setState(prevState => ({
-//                             ...prevState,
-//                             'successMessage': "Registration success"
-//                         }))
-//                         this.redirectToHome()
-//                         this.props.showError(null)
-//                     } else {
-//                         this.props.showError("Some error ocurred")
-//                     }
-//                 })
-//                 .catch(function (error) {
-//                     console.log(error);
-//                 })
-
-//         } else {
-//             this.props.showError('Please enter valid username and password')
-//         }
-//     }
-
-//     redirectToHome = () => {
-//         this.props.updateTitle('Home')
-//         this.props.history.push('/home')
-//     }
-
-//     handleSubmitClick = (e) => {
-//         e.preventDefault()
-//         if (this.state.password === this.state.confirmPassword) {
-//             this.sendDetailsToServer()
-//         } else {
-//             this.props.showError('Passwords do not match')
-//         }
-//     }
-
-//     redirectToLogin = () => {
-//         this.props.updateTitle('Login')
-//         this.props.history.push('/login')
-//     }
-
-//     render() {
-//         return (
-//             <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
-//                 <form>
-//                     <div className="form-group text-left">
-//                         <label htmlFor="exampleInputEmail1">Email</label>
-//                         <input type="email"
-//                                className="form-control"
-//                                id="email"
-//                                aria-describedby="emailHelp"
-//                                placeholder="Enter email"
-//                                value={this.state.email}
-//                                onChange={this.handleChange}
-//                         />
-//                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-//                     </div>
-//                     <div className="form-group text-left">
-//                         <label htmlFor="exampleInputUsername1">Username</label>
-//                         <input type="text"
-//                                className="form-control"
-//                                id="username"
-//                                placeholder="Username"
-//                                value={this.state.username}
-//                                onChange={this.handleChange}
-//                         />
-//                     </div>
-//                     <div className="form-group text-left">
-//                         <label htmlFor="exampleInputPassword1">Password</label>
-//                         <input type="password" 
-//                             className="form-control" 
-//                             id="password" 
-//                             placeholder="Password"
-//                             value={this.state.password}
-//                             onChange={this.handleChange}
-//                         />
-//                     </div>
-//                     <div className="form-group text-left">
-//                         <label htmlFor="exampleInputPassword1">Confirm Password</label>
-//                         <input type="password" 
-//                             className="form-control" 
-//                             id="confirmPassword" 
-//                             placeholder="Confirm Password"
-//                             value={this.state.confirmPassword}
-//                             onChange={this.handleChange}
-//                         />
-//                     </div>
-//                     <button
-//                         type="submit"
-//                         className="btn btn-primary"
-//                         onClick={this.handleSubmitClick}
-//                     >Register</button>
-//                 </form>
-//                 <div className="alert alert-success mt-2" style={{display: this.state.successMessage ? 'block' : 'none'}} role="alert">
-//                     {this.state.successMessage}
-//                 </div>
-//                 <div className="mt-2">
-//                     <span>Already have an account?</span>
-//                     <span className="loginText" onClick={() => this.redirectToLogin()}>Login here</span>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
 
 export default withRouter(RegisterForm)
