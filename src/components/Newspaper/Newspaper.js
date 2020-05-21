2e41e0f150099d45986d78bffb8250fee55e3dbe
Ezequiel Olea Figueroa <eze.olea.f@gmail.com>
2020-05-20 23:45:26 -0300

    Moved sections and newspapers to Materialize

--- src/components/Newspaper/Newspaper.js
+++ src/components/Newspaper/Newspaper.js
@@ -1,87 +1,195 @@
-import React, {useState} from 'react'
 import axios from 'axios'
 // import './Newspaper.css'
 import {API_BASE_URL} from '../../constants/apiConstants'
 import {withRouter} from 'react-router-dom'
 import SectionList from '../Sections/SectionList'
 
-class Newspaper extends React.Component {
-    constructor(props) {
-        super(props)
+// class Newspaper extends React.Component {
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
-            newspapers: [],
-            selectedNewspaper: null,
-            filteredNewspapers: [],
-            sections: [],
-            searchSections: false,
-            search: ""
-        }
+//         this.state = {
+//             newspapers: [],
+//             selectedNewspaper: null,
+//             filteredNewspapers: [],
+//             sections: [],
+//             searchSections: false,
+//             search: ""
+//         }
 
-        this.resetSections = this.resetSections.bind(this);
-        this.searchSections = this.searchSections.bind(this);
-    }
+//         this.resetSections = this.resetSections.bind(this);
+//         this.searchSections = this.searchSections.bind(this);
+//     }
 
-    searchSections = (id) => {
-        this.setState({
-            searchSections: true,
-            selectedNewspaper: id,
-          })
-    }
+//     searchSections = (id) => {
+//         this.setState({
+//             searchSections: true,
+//             selectedNewspaper: id,
+//           })
+//     }
 
-    resetSections = () => {
-        this.setState({
-            searchSections: false,
-            selectedNewspaper: null
-        })
-    }
+//     resetSections = () => {
+//         this.setState({
+//             searchSections: false,
+//             selectedNewspaper: null
+//         })
+//     }
 
-    render() {
-        const renderSections = this.state.searchSections;
-        return (
-            <div>
-                { this.state.searchSections ?
-                <SectionList newspaperId={this.state.selectedNewspaper} reset={this.resetSections} />
-                :
-                this.state.newspapers &&
-                this.state.newspapers.map( item =>
-                    <div className="card my-4" onClick={() => this.searchSections(item.id)} key={item.id}>
-                        <div className="card-body">
-                            {item.name} - {item.country}
-                        </div>
-                    </div>
-                )}
-            </div>
-        )
-    }
+//     render() {
+//         const renderSections = this.state.searchSections;
+//         return (
+//             <div>
+//                 { this.state.searchSections ?
+//                 <SectionList newspaperId={this.state.selectedNewspaper} reset={this.resetSections} />
+//                 :
+//                 this.state.newspapers &&
+//                 this.state.newspapers.map( item =>
+//                     <div className="card my-4" onClick={() => this.searchSections(item.id)} key={item.id}>
+//                         <div className="card-body">
+//                             {item.name} - {item.country}
+//                         </div>
+//                     </div>
+//                 )}
+//             </div>
+//         )
+//     }
 
-    componentDidMount = () => {
-        this.getNewspapers()
-    }
+//     componentDidMount = () => {
+//         this.getNewspapers()
+//     }
 
-    getNewspapers = () => {
-        const authToken = localStorage.getItem("token")
+//     getNewspapers = () => {
+//         const authToken = localStorage.getItem("token")
 
-        var myHeaders = new Headers()
-        myHeaders.append("Content-Type", "application/json")
-        myHeaders.append("Authorization", "bearer " + authToken)
-        const token = 'Bearer '.concat(authToken);
-        console.log("Bearer " + authToken)
+//         var myHeaders = new Headers()
+//         myHeaders.append("Content-Type", "application/json")
+//         myHeaders.append("Authorization", "bearer " + authToken)
+//         const token = 'Bearer '.concat(authToken);
+//         console.log("Bearer " + authToken)
 
-        axios.get(API_BASE_URL + 'newspapers', { headers: { Authorization: token } } )
-            .then(response => {
-                if(response.status === 200) {
-                    console.log(response.data)
-                    this.setState({ newspapers: response.data.items })
-                }
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
+//         axios.get(API_BASE_URL + 'newspapers', { headers: { Authorization: token } } )
+//             .then(response => {
+//                 if(response.status === 200) {
+//                     console.log(response.data)
+//                     this.setState({ newspapers: response.data.items })
+//                 }
+//             })
+//             .catch(function (error) {
+//                 console.log(error)
+//             })
 
-        console.log("Retrieve newspapers")
-    }
+//         console.log("Retrieve newspapers")
+//     }
+// }
 
+// export default Newspaper
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
 }
 
-export default Newspaper
+function NewspaperList(props) {
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    newspapers: [],
+    selectedNewspaper: null,
+    filteredNewspapers: [],
+    sections: [],
+    searchSections: false,
+    search: ""
+  })
+
+  const searchSections = (id) => {
+    setState( (prevState) => ({
+        ...prevState,
+        searchSections: true,
+        selectedNewspaper: id,
+    }))
+  }
+
+  const resetNewspapers = () => {
+    setState( (prevState) => ({
+      ...prevState,
+      searchSections: false,
+      selectedNewspaper: null,
+    }))
+  }
+
+  const getNewspapers = () => {
+    const authToken = localStorage.getItem("token")
+
+    var myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+    myHeaders.append("Authorization", "bearer " + authToken)
+    const token = 'Bearer '.concat(authToken);
+
+    axios.get(API_BASE_URL + 'newspapers', { headers: { Authorization: token } } )
+        .then((response) => {
+            if(response.status === 200) {
+              console.log(response);
+                setState(prevState => ({
+                  ...prevState,
+                  newspapers: response.data.items }))
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    console.log("Retrieve newspapers")
+}
+
+  React.useEffect(() => {
+    if (state.newspapers.length == 0) {
+      getNewspapers()
+    }
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      { !state.selectedNewspaper ?
+      <List component="nav" aria-label="main mailbox folders">
+        { state.newspapers.map( item =>
+          <ListItem button key={item.id} onClick={() => searchSections(item.id)} >
+            <ListItemText style={{ textAlign: 'center' }} primary={item.name} secondary={item.country} />
+          </ListItem>
+          
+        )}
+      </List>
+      :
+        <SectionList newspaperId={state.selectedNewspaper} reset={resetNewspapers} />
+      }
+      {/* <Divider />
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItemLink>
+      </List> */}
+    </div>
+  );
+}
+
+export default NewspaperList
--- src/components/Sections/SectionList.js
+++ src/components/Sections/SectionList.js
@@ -1,87 +1,224 @@
-import React, {useState} from 'react'
+// import React, {useState} from 'react'
 import axios from 'axios'
 // import './Newspaper.css'
 import {API_BASE_URL} from '../../constants/apiConstants'
 import {withRouter} from 'react-router-dom'
 import NewsList from '../News/NewsList'
 
-class SectionList extends React.Component {
-    constructor(props) {
-        super(props)
+// class SectionList extends React.Component {
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
-            selectedNewspaper: props.newspaperId,
-            sections: [],
-            selectedSection: null,
-            searchNews: false
-        }
+//         this.state = {
+//             selectedNewspaper: props.newspaperId,
+//             sections: [],
+//             selectedSection: null,
+//             searchNews: false
+//         }
 
-        this.searchNews = this.searchNews.bind(this)
-        this.resetNews = this.resetNews.bind(this)
+//         this.searchNews = this.searchNews.bind(this)
+//         this.resetNews = this.resetNews.bind(this)
+//     }
+
+//     searchNews = (id) => {
+//         this.setState(() => {
+//             return {
+//                 searchNews: true,
+//                 selectedSection: id,
+//             }
+//           })
+//     }
+
+//     resetNews = () => {
+//         this.setState(() => {
+//             return {
+//                 searchNews: false,
+//                 selectedSection: null
+//             }
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <br/><h3 onClick={this.props.reset}> Sections </h3><br/>
+//                 { this.state.searchNews ?
+//                 <NewsList sectionId={this.state.selectedSection} reset={this.resetNews} />
+//                 :
+//                 this.state.sections &&
+//                 this.state.sections.map( item =>
+//                     <div className="list-group-item list-group-item-action flex-column align-items-start" onClick={() => this.searchNews(item.id)} key={item.id}>
+//                         <div className="d-flex w-100 justify-content-between">
+//                         <h6 className="my-2">{item.name}&nbsp;&nbsp;&nbsp;&nbsp;</h6>
+//                         <small><a href="#" className="button">Follow</a></small>
+//                         </div>
+//                         <p className="mb-1">{item.description}</p>
+//                     </div>
+//                 )}
+//             </div>
+//         )
+//     }
+
+//     componentDidMount = () => {
+//         this.getSections()
+//     }
+
+//     getSections = () => {
+//         const authToken = localStorage.getItem("token")
+
+//         var myHeaders = new Headers()
+//         myHeaders.append("Content-Type", "application/json")
+//         myHeaders.append("Authorization", "bearer " + authToken)
+//         const token = 'Bearer '.concat(authToken);
+
+//         axios.get(API_BASE_URL + 'newspapers/' + this.state.selectedNewspaper, { headers: { Authorization: token } } )
+//             .then(response => {
+//                 if(response.status === 200) {
+//                     this.setState({ sections: response.data.sections })
+//                 }
+//             })
+//             .catch(function (error) {
+//                 console.log(error)
+//             })
+//     }
+
+// }
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Switch from '@material-ui/core/Switch';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+function SectionList(props) {
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    selectedNewspaper: props.newspaperId,
+    sections: [],
+    selectedSection: null,
+    searchNews: false,
+    filteredSections: [],
+    search: ""
+  })
+
+  const searchNews = (id) => {
+    setState((prevState) => ({
+      ...prevState,  
+      searchNews: true,
+      selectedSection: id,
+    }))
+  }
+
+  const [checked, setChecked] = React.useState({
+    sectionsFollowed: []
+  });
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.sectionsFollowed.indexOf(value);
+    const newChecked = [...checked.sectionsFollowed];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
     }
 
-    searchNews = (id) => {
-        this.setState(() => {
-            return {
-                searchNews: true,
-                selectedSection: id,
-            }
-          })
+    setChecked(newChecked);
+  };
+
+  const startChecked = (response) => {
+    
+  }
+
+  const resetSections = () => {
+    setState((prevState) => ({
+      ...prevState,  
+      searchNews: false,
+      selectedSection: null,
+    }))
+  }
+
+  const getSections = () => {
+    const authToken = localStorage.getItem("token")
+
+    var myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+    myHeaders.append("Authorization", "bearer " + authToken)
+    const token = 'Bearer '.concat(authToken);
+
+    axios.get(API_BASE_URL + 'newspapers/' + props.newspaperId, { headers: { Authorization: token } } )
+      .then(response => {
+          if(response.status === 200) {
+            console.log(response)
+            setState((prevState) => ({
+              ...prevState,
+              sections: response.data.sections
+            }))
+            startChecked(response)
+          }
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+  }
+
+  React.useEffect(() => {
+    if (state.sections.length == 0) {
+      getSections()
     }
+  }, []);
 
-    resetNews = () => {
-        this.setState(() => {
-            return {
-                searchNews: false,
-                selectedSection: null
-            }
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <br/><h3 onClick={this.props.reset}> Sections </h3><br/>
-                { this.state.searchNews ?
-                <NewsList sectionId={this.state.selectedSection} reset={this.resetNews} />
-                :
-                this.state.sections &&
-                this.state.sections.map( item =>
-                    <div className="list-group-item list-group-item-action flex-column align-items-start" onClick={() => this.searchNews(item.id)} key={item.id}>
-                        <div className="d-flex w-100 justify-content-between">
-                        <h6 className="my-2">{item.name}&nbsp;&nbsp;&nbsp;&nbsp;</h6>
-                        <small><a href="#" className="button">Follow</a></small>
-                        </div>
-                        <p className="mb-1">{item.description}</p>
-                    </div>
-                )}
-            </div>
-        )
-    }
-
-    componentDidMount = () => {
-        this.getSections()
-    }
-
-    getSections = () => {
-        const authToken = localStorage.getItem("token")
-
-        var myHeaders = new Headers()
-        myHeaders.append("Content-Type", "application/json")
-        myHeaders.append("Authorization", "bearer " + authToken)
-        const token = 'Bearer '.concat(authToken);
-
-        axios.get(API_BASE_URL + 'newspapers/' + this.state.selectedNewspaper, { headers: { Authorization: token } } )
-            .then(response => {
-                if(response.status === 200) {
-                    this.setState({ sections: response.data.sections })
-                }
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
-    }
-
+  return (
+    <div className={classes.root}>
+      { !state.selectedSection ?
+      <List component="nav" aria-label="sections">
+        { state.sections.map( item =>
+          <ListItem button key={item.id} onClick={() => searchNews(item.id)} >
+            <ListItemText style={{ textAlign: 'center' }} primary={item.name} secondary={item.description} />
+            <ListItemSecondaryAction>
+            <Switch
+              edge="end"
+              onChange={handleToggle(item.id)}
+              checked={checked.sectionsFollowed.indexOf(item.id) !== -1}
+              inputProps={{ 'aria-labelledby': 'switch-list-label-wifi' }}
+            />
+            </ListItemSecondaryAction>
+          </ListItem>
+          
+        )}
+      </List>
+      :
+        "a"
+        // <SectionList newspaperId={state.selectedNewspaper} reset={resetSections} />
+      }
+      {/* <Divider />
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItemLink>
+      </List> */}
+    </div>
+  );
 }
 
 export default SectionList
