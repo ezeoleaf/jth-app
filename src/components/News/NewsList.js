699a8e6d531c947120a5e0692a7e82bfeeca5717
Ezequiel Olea Figueroa <eze.olea.f@gmail.com>
2020-05-23 12:07:28 -0300

    Added news

--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -1,62 +1,78 @@
-import React, {useState} from 'react'
+import React from 'react'
 import axios from 'axios'
 import {API_BASE_URL} from '../../constants/apiConstants'
-import {withRouter} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-class NewsList extends React.Component {
-    constructor(props) {
-        super(props)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-        this.state = {
-            selectedSection: props.sectionId,
-            news: [],
+function NewsList(props) {
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    selectedSection: props.sectionId,
+    news: [],
+  })
+
+  const getNews = () => {
+    const authToken = localStorage.getItem("token")
+    const token = 'Bearer '.concat(authToken);
+
+    axios.get(API_BASE_URL + 'news/' + state.selectedSection, { headers: { Authorization: token } } )
+      .then(response => {
+          if(response.status === 200) {
+            setState((prevState) => ({
+              ...prevState,
+              news: response.data
+            }))
+          }
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+  }
+
+  React.useEffect(() => {
+    if (state.news.length == 0) {
+      getNews()
+    }
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="news">
+        <ListItem button onClick={props.reset}>
+          <ListItemText style={{ textAlign: 'center' }} primary="Return" />
+          <ListItemSecondaryAction></ListItemSecondaryAction>
+        </ListItem>
+        <Divider />
+        { state.news ?
+          state.news.map( (item, ix) =>
+            <ListItem button key={ix} >
+              <ListItemText style={{ textAlign: 'center' }} primary={item.title} secondary={item.description} />
+            </ListItem>
+            
+          )
+        :
+        <List component="nav" aria-label="news">
+          <ListItem>
+            <ListItemText style={{ textAlign: 'center' }} primary="No news available" />
+          </ListItem>
+        </List>
         }
-    }
-
-    render() {
-        return (
-            <div>
-                <br/><h3 onClick={this.props.reset}> News </h3><br/>
-                <div className="list-group">
-                {
-                this.state.news &&
-                this.state.news.map( (item, ix) =>
-                    <div className="list-group-item list-group-item-action flex-column align-items-start" key={ix}>
-                        <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">{item.title}</h5>
-                        <small>{item.newspaper}</small>
-                        </div>
-                        <p className="mb-1">{item.description}</p>
-                    </div>
-                )}
-                </div>
-            </div>
-        )
-    }
-
-    componentDidMount = () => {
-        this.getNews()
-    }
-
-    getNews = () => {
-        const authToken = localStorage.getItem("token")
-
-        var myHeaders = new Headers()
-        myHeaders.append("Content-Type", "application/json")
-        myHeaders.append("Authorization", "bearer " + authToken)
-        const token = 'Bearer '.concat(authToken);
-
-        axios.get(API_BASE_URL + 'news/' + this.state.selectedSection, { headers: { Authorization: token } } )
-            .then(response => {
-                if(response.status === 200) {
-                    this.setState({ news: response.data })
-                }
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
-    }
-
+      </List>
+    </div>
+  );
 }
 
 export default NewsList
--- src/components/Sections/SectionList.js
+++ src/components/Sections/SectionList.js
@@ -1,94 +1,10 @@
-// import React, {useState} from 'react'
-import axios from 'axios'
-// import './Newspaper.css'
-import {API_BASE_URL} from '../../constants/apiConstants'
-import {withRouter} from 'react-router-dom'
-import NewsList from '../News/NewsList'
-
-// class SectionList extends React.Component {
-//     constructor(props) {
-//         super(props)
-
-//         this.state = {
-//             selectedNewspaper: props.newspaperId,
-//             sections: [],
-//             selectedSection: null,
-//             searchNews: false
-//         }
-
-//         this.searchNews = this.searchNews.bind(this)
-//         this.resetNews = this.resetNews.bind(this)
-//     }
-
-//     searchNews = (id) => {
-//         this.setState(() => {
-//             return {
-//                 searchNews: true,
-//                 selectedSection: id,
-//             }
-//           })
-//     }
-
-//     resetNews = () => {
-//         this.setState(() => {
-//             return {
-//                 searchNews: false,
-//                 selectedSection: null
-//             }
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <br/><h3 onClick={this.props.reset}> Sections </h3><br/>
-//                 { this.state.searchNews ?
-//                 <NewsList sectionId={this.state.selectedSection} reset={this.resetNews} />
-//                 :
-//                 this.state.sections &&
-//                 this.state.sections.map( item =>
-//                     <div className="list-group-item list-group-item-action flex-column align-items-start" onClick={() => this.searchNews(item.id)} key={item.id}>
-//                         <div className="d-flex w-100 justify-content-between">
-//                         <h6 className="my-2">{item.name}&nbsp;&nbsp;&nbsp;&nbsp;</h6>
-//                         <small><a href="#" className="button">Follow</a></small>
-//                         </div>
-//                         <p className="mb-1">{item.description}</p>
-//                     </div>
-//                 )}
-//             </div>
-//         )
-//     }
-
-//     componentDidMount = () => {
-//         this.getSections()
-//     }
-
-//     getSections = () => {
-//         const authToken = localStorage.getItem("token")
-
-//         var myHeaders = new Headers()
-//         myHeaders.append("Content-Type", "application/json")
-//         myHeaders.append("Authorization", "bearer " + authToken)
-//         const token = 'Bearer '.concat(authToken);
-
-//         axios.get(API_BASE_URL + 'newspapers/' + this.state.selectedNewspaper, { headers: { Authorization: token } } )
-//             .then(response => {
-//                 if(response.status === 200) {
-//                     this.setState({ sections: response.data.sections })
-//                 }
-//             })
-//             .catch(function (error) {
-//                 console.log(error)
-//             })
-//     }
-
-// }
-
 import React from 'react';
+import axios from 'axios'
+import {API_BASE_URL} from '../../constants/apiConstants'
+import NewsList from '../News/NewsList'
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
@@ -213,15 +129,6 @@ function SectionList(props) {
       :
         <NewsList sectionId={state.selectedSection} reset={resetSections} />
       }
-      {/* <Divider />
-      <List component="nav" aria-label="secondary mailbox folders">
-        <ListItem button>
-          <ListItemText primary="Trash" />
-        </ListItem>
-        <ListItemLink href="#simple-list">
-          <ListItemText primary="Spam" />
-        </ListItemLink>
-      </List> */}
     </div>
   );
 }
